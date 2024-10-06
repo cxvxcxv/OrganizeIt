c73e01af6a19ee4051e10434514c2d4f9723f515
unknown <ami145388@gmail.com>
2024-10-06 15:01:10 +0500

    upd: .env (added: port) & main.ts (added: cors & global prefix & disabled x-powered-by header. upd: port)

--- server/src/constants/env.constants.ts
+++ server/src/constants/env.constants.ts
@@ -7,4 +7,5 @@ export const JWT_SECRET = 'JWT_SECRET';
 export const JWT_EXP = 'JWT_EXP';
 export const DOMAIN = 'DOMAIN';
 export const ORIGIN = 'ORIGIN';
+export const PORT = 'PORT';
 export const NODE_ENV = 'NODE_ENV';
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,22 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { ORIGIN, PORT } from './constants/env.constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get<ConfigService>(ConfigService);
 
-  app.use(cookieParser());
+  app.getHttpAdapter().getInstance().disable('x-powered-by'); //disables 'x-powered-by' in headers for stronger security
+  app.use(cookieParser()); //vital for cookies work
+  app.enableCors({
+    origin: configService.get(ORIGIN),
+    credentials: true, //allows the browser to include cookies and other credentials in cross-origin requests
+    exposedHeaders: 'set-cookie', //allows client-side scripts to read the set-cookie header in cross-origin requests
+  });
+  app.setGlobalPrefix('/api');
 
-  await app.listen(3000);
+  await app.listen(configService.get(PORT) | 3001);
 }
 bootstrap();
